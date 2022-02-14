a7560e49e81d21ae8160c0dd672bc9c083b9900e
Asdrubal Antonio Ramírez Botero <aramirez@udemedellin.edu.co>
2022-02-14 14:45:58 -0500

    commit

---
+++ Assets/Scripts/controlScenas.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class controlScenas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void CambiarScena(string scena){
+        if(scena == "WelcomeSpace"){
+            SceneManager.LoadScene("WelcomeSpace");
+        }else if( scena == "VistaGeneral"){
+            SceneManager.LoadScene("VistaGeneral");
+        }else if( scena == "VistaDeSala"){
+            SceneManager.LoadScene("VistaDeSala");
+        }else if( scena == "VistaDeChico"){
+            SceneManager.LoadScene("VistaDeChico");
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
